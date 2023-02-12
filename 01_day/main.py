8c3c73e2cc8d464083c33346be502e12dd7fd5d9
benhendriks <benhendriks@protonmail.com>
2023-02-12 15:30:12 +0100

    Finished day one & two

---
+++ 01_day/main.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generater')
+city = input('What`s the Name of the City you grow up in? \n')
+pet = input('What`s you pat`s name? \n')
+print('Your Band Name could be ' + city + '' + pet)
