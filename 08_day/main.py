f49d85509af56f70b3767bf80188fbf9b8e9629f
Benjamin Steidl <benhendriks@protonmail.com>
2025-03-07 22:59:06 +0100

    Added function for encode & decode

--- 08_day/main.py
+++ 08_day/main.py
@@ -8,14 +8,38 @@
     
 #life_in_weeks(user_input)
 
-alaphabet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n").lower()
 text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number:\n"))
 
 def encrypt(original_text, shift_amount):
+    cipher_text = ""
+
     for letter in original_text:
-        shifted_position = alphabet.index(letter)
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
+
+    print(f"Here is the encoded result: {cipher_text}")
+
+#encrypt(original_text=text, shift_amount=shift)
+
+def ceaser(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+
+    for letter in original_text:
+        if encode_or_decode == "decode":
+            shift_amount *= -1 
+
+        shifted_position = alphabet.index(letter) - shift_amount
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
+
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+ceaser(text, shift, direction)
+
 
 
