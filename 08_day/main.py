7f10e3f9e9c556b29f8b9fae425ef8a712914b4b
Benjamin Steidl <benhendriks@protonmail.com>
2025-03-08 21:42:35 +0100

    Added while loop & user input

--- 08_day/main.py
+++ 08_day/main.py
@@ -10,9 +10,6 @@
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n").lower()
-text = input("Type your message: \n").lower()
-shift = int(input("Type the shift number:\n"))
 
 def encrypt(original_text, shift_amount):
     cipher_text = ""
@@ -28,18 +25,32 @@ def encrypt(original_text, shift_amount):
 
 def ceaser(original_text, shift_amount, encode_or_decode):
     output_text = ""
+   
+    if encode_or_decode == "decode":
+                shift_amount *= -1 
 
     for letter in original_text:
-        if encode_or_decode == "decode":
-            shift_amount *= -1 
-
-        shifted_position = alphabet.index(letter) - shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
+        if letter not in alphabet:
+            output_text += letter
+        else:  
+            shifted_position = alphabet.index(letter) - shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
 
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-ceaser(text, shift, direction)
+should_continue = True
 
+while should_continue:
 
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n").lower()
+    text = input("Type your message: \n").lower()
+    shift = int(input("Type the shift number:\n"))
 
+    ceaser(text, shift, direction)
+
+    restart = input(f"Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    if restart == 'no':
+        should_continue = False
+        print("Goodbye")
+        
